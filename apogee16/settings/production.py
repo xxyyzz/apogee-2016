edd0a0c411ab19bbb005d61be4d7e6e59f214301
Nikhil Verma <noteenikhil@yahoo.in>
2015-09-22 20:51:09 +0530

    updated settings

---
+++ apogee16/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DATABASE_USERNAME,            # Or path to database file if using sqlite3.
+        'USER': DATABASE_USER,                # Not used with sqlite3.
+        'PASSWORD': DATABASE_PASSWORD,        # Not used with sqlite3.
+        'HOST': '',                           # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                           # Set to empty string for default. Not used with sqlite3.
+    }
+}
