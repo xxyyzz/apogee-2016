265b7f473200633a69e93108b666f7e7fd16faf7
Nikhil Verma <noteenikhil@yahoo.in>
2015-09-22 22:59:37 +0530

    added college to Participant

--- registrations/admin.py
+++ registrations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from registrations.models import *
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Paper)
+admin.site.register(Category)
+admin.site.register(Association)
+admin.site.register(Participant)
+admin.site.register(College)
--- registrations/models.py
+++ registrations/models.py
@@ -11,6 +11,8 @@ class Project(models.Model):
     members = models.ManyToManyField('Participant', related_name='members')
     upload = models.FileField()
     stub = models.CharField(max_length=5, unique=True)
+    def __unicode__(self):
+        return self.name
 
 class Paper(models.Model):
     name = models.CharField(max_length=500)
@@ -20,6 +22,8 @@ class Paper(models.Model):
     address = models.TextField()
     author = models.ForeignKey('Participant', related_name='authors')
     co_author = models.ForeignKey('Participant', related_name='co_authors')
+    def __unicode__(self):
+        return self.name
 
 class Category(models.Model):
     MODELS = (
@@ -29,14 +33,25 @@ class Category(models.Model):
     )
     name = models.CharField(max_length=200)
     model = models.CharField(max_length=1, choices=MODELS)
+    class Meta:
+        verbose_name_plural = 'categories'
+    def __unicode__(self):
+        return self.name
 
 class Association(models.Model):
     name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
 
 class Participant(models.Model):
     name = models.CharField(max_length=200)
     phone = models.BigIntegerField()
     email = models.EmailField(unique=True)
+    college = models.ForeignKey('College')
+    def __unicode__(self):
+        return self.name
 
 class College(models.Model):
     name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
