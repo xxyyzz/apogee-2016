8812ee87dc0d234eec344ce1a25a885729c76594
Pranjal <g31.pranjal@gmail.com>
2015-09-23 11:12:31 +0530

    portal views

--- registrations/views.py
+++ registrations/views.py
@@ -1,3 +1,10 @@
+from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.shortcuts import render
+from registrations.models import *
+from django.template import Context
+from django.http import HttpResponse
+
+def home(request) :
+	return render(request, "portal/index.html")
 
 # Create your views here.
