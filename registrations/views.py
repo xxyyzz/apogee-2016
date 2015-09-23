524675bdba550d1e39ecf0bc9127b6961ccab574
Pranjal <g31.pranjal@gmail.com>
2015-09-24 01:47:12 +0530

    form context added

--- registrations/views.py
+++ registrations/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from registrations.models import *
 from django.template import Context
 from django.http import HttpResponse
+from registrations.models import *
 
 def home(request) :
 	return render(request, "portal/index.html")
@@ -11,13 +12,21 @@ def papersInstructions(request) :
 	return render(request, "portal/partials/papers_main.html")
 
 def papersForm(request) :
-	return render(request, "portal/partials/papers_form.html")
+	context = {};
+	a = Category.objects.filter(model__in= {'Paper', 'Both'})
+	context['categories'] = list(a)
+	return render(request, "portal/partials/papers_form.html",context)
 
 def projectsInstructions(request) :
 	return render(request, "portal/partials/projects_main.html")
 
 def projectsForm(request) :
-	return render(request, "portal/partials/projects_form.html")
+	context = {};
+	a = Category.objects.filter(model__in= {'Project', 'Both'})
+	context['categories'] = list(a)
+	b = Association.objects.all()
+	context['assocs'] = list(b)
+	return render(request, "portal/partials/projects_form.html", context)
 
 def checkForm(request) :
 	return render(request, "portal/partials/check_form.html")
