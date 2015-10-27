79c8ba9aad63c8198f84672d486f1f990d016c28
Nikhil Verma <noteenikhil@yahoo.in>
2015-10-27 14:29:09 +0530

    tweaked excel again

--- cms/views.py
+++ cms/views.py
@@ -21,19 +21,27 @@ def paper_stats_xlsx(request):
     workbook = xlsxwriter.Workbook(output)
     worksheet = workbook.add_worksheet('new-spreadsheet')
     date_format = workbook.add_format({'num_format': 'mmmm d yyyy'})
+
+    worksheet.write(0, 0, "Paper ID")
+    worksheet.write(0, 1, "Paper Name")
+    worksheet.write(0, 2, "Category")
+    worksheet.write(0, 3, "Reference Code")
+    worksheet.write(0, 4, "Author")
+    worksheet.write(0, 5, "Co-Author")
+        
     for i, row in enumerate(data):
         """for each object in the date list, attribute1 & attribute2
         are written to the first & second column respectively,
         for the relevant row. The 3rd arg is a failure message if
         there is no data available"""
 
-        worksheet.write(i, 0, getattr(row['obj'], 'id', 'NA'))
-        worksheet.write(i, 1, getattr(row['obj'], 'name', 'NA'))
-        worksheet.write(i, 2, getattr(row['obj'].category , 'name', 'NA'))
-        worksheet.write(i, 3, getattr(row['obj'], 'stub', 'NA'))
-        worksheet.write(i, 4, getattr(row['obj'].author , 'name', 'NA'))
-        worksheet.write(i, 5, getattr(row['obj'].co_author , 'name', 'NA'))
-        
+        worksheet.write(i+1, 0, getattr(row['obj'], 'id', 'NA'))
+        worksheet.write(i+1, 1, getattr(row['obj'], 'name', 'NA'))
+        worksheet.write(i+1, 2, getattr(row['obj'].category , 'name', 'NA'))
+        worksheet.write(i+1, 3, getattr(row['obj'], 'stub', 'NA'))
+        worksheet.write(i+1, 4, getattr(row['obj'].author , 'name', 'NA'))
+        worksheet.write(i+1, 5, getattr(row['obj'].co_author , 'name', 'NA'))
+
     workbook.close()
     filename = 'ExcelReport.xlsx'
     output.seek(0)
