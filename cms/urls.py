bc36336b64005594ce363189154b6bd6bda0865e
Nikhil Verma <noteenikhil@yahoo.in>
2015-11-03 22:00:37 +0530

    added project excel

--- cms/urls.py
+++ cms/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from cms import views
 
 urlpatterns = [
-    url(r'^paperstats/', views.paper_stats_xlsx),
+    url(r'^paperxlsx/', views.paper_stats_xlsx),
+    url(r'^projectxlsx/', views.project_stats_xlsx),
 ]
--- cms/views.py
+++ cms/views.py
@@ -81,3 +81,67 @@ def paper_stats_xlsx(request):
     response = HttpResponse(output.read(), content_type="application/ms-excel")  
     response['Content-Disposition'] = 'attachment; filename=%s' % filename
     return response
+
+def project_stats_xlsx(request):
+    from django.http import HttpResponse, HttpResponseRedirect  
+    import xlsxwriter
+
+    try:  
+        import cStringIO as StringIO
+    except ImportError:  
+        import StringIO
+    a_list = []
+
+    from registrations.models import Project
+    papers = Project.objects.all()
+
+    for p in papers:
+        a_list.append({'obj': p})
+    data = sorted(a_list, key=lambda k: k['obj'].assoc)
+    output = StringIO.StringIO()
+    workbook = xlsxwriter.Workbook(output)
+    worksheet = workbook.add_worksheet('new-spreadsheet')
+    date_format = workbook.add_format({'num_format': 'mmmm d yyyy'})
+
+    worksheet.write(0, 0, "Project ID")
+    worksheet.write(0, 1, "Project Name")
+    worksheet.write(0, 2, "Category")
+    worksheet.write(0, 3, "Assoc")
+    worksheet.write(0, 4, "Reference Code")
+    worksheet.write(0, 5, "Leader Name")
+    worksheet.write(0, 6, "Leader Phone")
+    worksheet.write(0, 7, "Leader Email")
+    worksheet.write(0, 8, "Leader College")
+    for i in range(5):
+        worksheet.write(0, 9+4*i, "Member "+str(i+1)+" Names")
+        worksheet.write(0, 10+4*i, "Member "+str(i+1)+" Phone")
+        worksheet.write(0, 11+4*i, "Member "+str(i+1)+" Email")
+        worksheet.write(0, 12+4*i, "Member "+str(i+1)+" College")
+        
+    for i, row in enumerate(data):
+        """for each object in the date list, attribute1 & attribute2
+        are written to the first & second column respectively,
+        for the relevant row. The 3rd arg is a failure message if
+        there is no data available"""
+
+        worksheet.write(i+1, 0, deepgetattr(row['obj'], 'id', 'NA'))
+        worksheet.write(i+1, 1, deepgetattr(row['obj'], 'name', 'NA'))
+        worksheet.write(i+1, 2, deepgetattr(row['obj'], 'category.name', 'NA'))
+        worksheet.write(i+1, 3, deepgetattr(row['obj'], 'assoc.name', 'NA'))
+        worksheet.write(i+1, 4, deepgetattr(row['obj'], 'stub', 'NA'))
+        worksheet.write(i+1, 5, deepgetattr(row['obj'], 'leader.name', 'NA'))
+        worksheet.write(i+1, 6, deepgetattr(row['obj'], 'leader.phone', 'NA'))
+        worksheet.write(i+1, 7, deepgetattr(row['obj'], 'leader.email', 'NA'))
+        worksheet.write(i+1, 8, deepgetattr(row['obj'], 'leader.college.name', 'NA'))
+        for n, member in enumerate(row['obj'].members.all()):
+            worksheet.write(i+1, 9+4*n, deepgetattr(member, 'name', 'NA'))
+            worksheet.write(i+1, 10+4*n, deepgetattr(member, 'phone', 'NA'))
+            worksheet.write(i+1, 11+4*n, deepgetattr(member, 'email', 'NA'))
+            worksheet.write(i+1, 12+4*n, deepgetattr(member, 'college.name', 'NA'))
+
+    workbook.close()
+    filename = 'ExcelReport.xlsx'
+    output.seek(0)
+    response = HttpResponse(output.read(), content_type="application/ms-excel")  
+    response['Content-Disposition'] = 'attachment; filename=%s' % filename
+    return response
