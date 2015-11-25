fa52c6d1b10654d7d50eee7c8c69e5fab3a70c56
Nikhil Verma <noteenikhil@yahoo.in>
2015-11-26 01:19:58 +0530

    paper management v3

--- cms/urls.py
+++ cms/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^paperxlsx/$', utilities.paper_stats_xlsx),
     url(r'^projectxlsx/$', utilities.project_stats_xlsx),
     url(r'^papers/$', views.paper_home, name='paper_home'),
-    url(r'^papers/initial/$', views.paper_initial, name='paper_initial'),
+    url(r'^papers/(?:(?P<status>\d)/)$', views.paper_home, name='paper_home'),
+    # url(r'^papers/initial/$', views.paper_initial, name='paper_initial'),
     url(r'^projects/$', views.project_home, name='project_home'),
 ]
--- cms/views.py
+++ cms/views.py
@@ -20,17 +20,41 @@ def login(request):
             # Return an 'invalid login' error message.
             return render(request, 'cms/paper_home.html', {"status": 0})
     return render(request, 'cms/paper_home.html')
-def paper_home(request):
-    papers = Paper.objects.all()
+# def paper_home(request):
+#     papers = Paper.objects.all()
+#     context = {
+#     'papers': papers,
+#     }
+#     return render(request, 'cms/paper_home.html', context)
+def paper_home(request, status=None):
+    if request.POST:
+        if "initial" in request.POST:
+            for paperid in request.POST.getlist("qualifiers"):
+                paper = Paper.objects.get(id=paperid)
+                paper.status = "0"
+                paper.save()
+        if "round1" in request.POST:
+            for paperid in request.POST.getlist("qualifiers"):
+                paper = Paper.objects.get(id=paperid)
+                paper.status = "1"
+                paper.save()
+        if "round2" in request.POST:
+            for paperid in request.POST.getlist("qualifiers"):
+                paper = Paper.objects.get(id=paperid)
+                paper.status = "2"
+                paper.save()
+    if status == None:
+        papers = Paper.objects.all()
+    if status == '0':
+        papers = Paper.objects.filter(status='0')
+    if status == '1':
+        papers = Paper.objects.filter(status='1')
+    if status == '2':
+        papers = Paper.objects.filter(status='2')
     context = {
     'papers': papers,
+    'status': status,
     }
     return render(request, 'cms/paper_home.html', context)
-def paper_initial(request):
-    papers = Paper.objects.all()
-    context = {
-    'papers': papers,
-    }
-    return render(request, 'cms/paper_all.html', context)
 def project_home(request):
     return render(request, 'cms/paper_home.html')
