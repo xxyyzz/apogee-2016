289d08d2ca5bcc9c76a8ecf184d8007376dd3e8d
satwik77 <satwik55@gmail.com>
2015-12-29 00:43:06 +0530

    tab admin QuerySet

--- Event/admin.py
+++ Event/admin.py
@@ -3,7 +3,7 @@ from .models import events, EventCategory, Tag, Heading, Tabs, organization
 # Register your models here.
 
 class EventAdmin(admin.ModelAdmin):
-	list_display = ['id', 'name', 'category', 'is_kernel']
+	list_display = ['id', 'name','org', 'category', 'is_kernel']
 	list_display_links = ['name']
 	# list_filter = ['category']
 	search_fields = ['name']
@@ -19,18 +19,46 @@ class EventAdmin(admin.ModelAdmin):
 	    given_org = organization.objects.get(user= request.user)
 	    return qs.filter(org=given_org)
 
+
+class TabAdmin(admin.ModelAdmin):
+	list_display = ['heading', 'event']
+	# list_display_links = ['name']
+	# list_filter = ['category']
+	# search_fields = ['event.name']
+	def get_queryset(self, request):
+	    # """Limit Pages to those that belong to the request's user."""
+	    qs = super(TabAdmin, self).get_queryset(request)
+	    if request.user.is_superuser:
+	        # It is mine, all mine. Just return everything.
+	        return qs
+	    # Now we just add an extra filter on the queryset and
+	    # we're done. Assumption: Page.owner is a foreignkey
+	    # to a User.
+	    given_org = organization.objects.get(user= request.user)
+	    events_list = events.objects.filter(org=given_org)
+	    tabs_list=qs.filter(event=events_list[0])
+	    c=0
+	    for event in events_list:
+	    	if c==0:
+	    		continue
+    		c+=1
+	    	tabs_list= tabs_list + qs.filter(event= event)
+
+	    return tabs_list
+
+
 class EventCategoryAdmin(admin.ModelAdmin):
 	list_display = ('name','weight',)
 	list_editable = ('weight',)    
 
 
 class OrgAdmin(admin.ModelAdmin):
-	list_display = ('name',)
+	list_display = ('user','name','cord')
 # admin.site.register(Category)
 admin.site.register(events, EventAdmin)
 admin.site.register(EventCategory, EventCategoryAdmin)
 admin.site.register(Tag)
 admin.site.register(Heading)
-admin.site.register(Tabs)
+admin.site.register(Tabs, TabAdmin)
 admin.site.register(organization, OrgAdmin)
 # admin.site.register(Notification, NotificationAdmin)
