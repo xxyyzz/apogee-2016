79e5822abf9493d80e919a2c7be75040c06195a4
satwik77 <satwik55@gmail.com>
2016-01-15 19:58:12 +0530

    minor bug fix

--- Event/urls.py
+++ Event/urls.py
@@ -1,7 +1,7 @@
 from Event import views
 from django.conf.urls import url
 urlpatterns = [
-    url(r'^get_event/(?P<id>.*)/$', views.geteventdata, name='get'),
+    url(r'^get_event/$', views.geteventdata, name='get'),
     # url(r'^notify/(?P<notification_id>[0-9]+)/$', views.getnotificationdata, name='notifications'),
     # url(r'^category/(?P<category>.*)/$', views.categorywise, name='get'),
     url(r'^summary/$', views.summary, name='summary'),
