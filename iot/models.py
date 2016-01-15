5e6d4e94b1232c954ebdb4ba8604e7667667c1dd
PrateekGupta1509 <prateek.g1509@gmail.com>
2016-01-15 20:26:20 +0530

    iot

--- iot/models.py
+++ iot/models.py
@@ -4,23 +4,10 @@ from django.template.defaultfilters import slugify
 # Create your models here.
 
 def upload_iot(self, filename):
-    slugified_category = slugify(self.get_problem_statement_display())
-    path = 'iot-submission/%s/%s' % (slugified_category, filename)
+    path = 'iot-submission/%s' % ( filename)
     return path
 
 class IotSubmission(models.Model):
-    STATEMENTS = (
-    	('1', 'Shit1'),
-    	('2', 'Shit2'),
-    	('3', 'Shit3'),
-    	('4', 'Shit4'),
-    	('5', 'Shit5'),
-    	('6', 'Shit6'),
-    	('7', 'Shit7'),
-    	('8', 'Shit8'),
-    	('9', 'Shit9'),
-    )
-    #problem_statement = models.CharField(max_length=500, choices=STATEMENTS)
     leader = models.ForeignKey('Participant', related_name='leaders')
     # college = models.ForeignKey('College')
     members = models.ManyToManyField('Participant', related_name='members', blank='True')
@@ -30,10 +17,10 @@ class IotSubmission(models.Model):
 
 
 class Participant(models.Model):
-	name = models.CharField(max_length=200)
-	phone = models.BigIntegerField()
-	email = models.EmailField(unique=True)
-	college = models.CharField(max_length=200)
-	yos = models.PositiveSmallIntegerField()
-	def __str__(self):
-		return self.name
+    name = models.CharField(max_length=200)
+    phone = models.BigIntegerField()
+    email = models.EmailField(unique=True)
+    college = models.CharField(max_length=200)
+    yos = models.PositiveSmallIntegerField()
+    def __str__(self):
+        return self.name
