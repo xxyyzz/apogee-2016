310e73d0e7de5ddd146061a79de2a1804eb48680
Nikhil Verma <noteenikhil@yahoo.in>
2016-01-19 00:04:13 +0530

    add backend to admin

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from backend.models import *
 # Register your models here.
+
+admin.site.register(Participant)
+admin.site.register(Team)
+admin.site.register(College)
