6db765ed4bca7bb1d09109656701bdb275477c6f
Nikhil Verma <noteenikhil@yahoo.in>
2016-01-22 21:50:39 +0530

    filter_horizontal

--- backend/admin.py
+++ backend/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from backend.models import *
 # Register your models here.
 
-admin.site.register(Participant)
-admin.site.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    filter_horizontal = ['members']
+
+class ParticipantAdmin(admin.ModelAdmin):
+    filter_horizontal = ['events', 'teams']
+
+admin.site.register(Participant, ParticipantAdmin)
+admin.site.register(Team, TeamAdmin)
 admin.site.register(College)
