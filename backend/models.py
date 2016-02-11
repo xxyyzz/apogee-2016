74635f8d583eb7ba3e6454a14c096bb94748317f
Nikhil Verma <noteenikhil@yahoo.in>
2016-02-12 02:00:20 +0530

    added models for ems. Yes, planning models takes time

--- backend/models.py
+++ backend/models.py
@@ -5,61 +5,66 @@ from ckeditor.fields import RichTextField
 # Create your models here.
 
 class Participant(models.Model):
-	GENDERS = (
-		('M', 'Male'),
-		('F', 'Female'),
-		# ('O', 'Other'),
-	)
-	user = models.OneToOneField(User, null=True)
-	aadhaar = models.CharField(max_length=8, null=True, blank=True, default=None, unique=True)
-	name = models.CharField(max_length=200)
-	gender = models.CharField(max_length=1, choices=GENDERS)
-	college = models.ForeignKey('College')
-	city = models.CharField(max_length=20)
-	phone_one = models.BigIntegerField()
-	phone_two = models.BigIntegerField(null=True, blank=True)
-	email_id = models.EmailField(unique=True)
-	email_verified = models.BooleanField(default=False)
-	email_token = models.CharField(max_length=32, null=True, blank=True)
-	social_link = models.CharField(max_length=300, null=True, blank=True)
-	pcr_approval = models.BooleanField(default=False)
-	events = models.ManyToManyField('Event.Event', blank=True)
-	fee_paid = models.BooleanField(default=False)
-	timestamp = models.DateTimeField(auto_now_add=True)
+    GENDERS = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        # ('O', 'Other'),
+    )
+    is_bitsian = models.BooleanField(default=False)
+    user = models.OneToOneField(User, null=True)
+    aadhaar = models.CharField(max_length=8, null=True, blank=True, default=None, unique=True)
+    name = models.CharField(max_length=200)
+    gender = models.CharField(max_length=1, choices=GENDERS)
+    college = models.ForeignKey('College')
+    city = models.CharField(max_length=20)
+    phone_one = models.BigIntegerField()
+    phone_two = models.BigIntegerField(null=True, blank=True)
+    email_id = models.EmailField(unique=True)
+    email_verified = models.BooleanField(default=False)
+    email_token = models.CharField(max_length=32, null=True, blank=True)
+    social_link = models.CharField(max_length=300, null=True, blank=True)
+    pcr_approval = models.BooleanField(default=False)
+    events = models.ManyToManyField('Event.Event', blank=True)
+    fee_paid = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
-	teams = models.ManyToManyField('Team', blank=True)
+    teams = models.ManyToManyField('Team', blank=True)
 
-	bank_ifsc = models.CharField(max_length=11)
-	bank_name = models.CharField(max_length=200)
-	bank_account_no = models.CharField(max_length=20)
-	address = models.TextField(max_length=1000)
+    bank_ifsc = models.CharField(max_length=11)
+    bank_name = models.CharField(max_length=200)
+    bank_account_no = models.CharField(max_length=20)
+    address = models.TextField(max_length=1000)
 
 
-	# firewallzo = models.BooleanField('passed firewallz', default=False)
-	# firewallzi = models.BooleanField('passed inner booth', default=False)
-	# recnacc = models.BooleanField('passed recnacc', default=False)
-	# controlz = models.BooleanField('passed controlz', default=False)
+    # firewallzo = models.BooleanField('passed firewallz', default=False)
+    # firewallzi = models.BooleanField('passed inner booth', default=False)
+    # recnacc = models.BooleanField('passed recnacc', default=False)
+    # controlz = models.BooleanField('passed controlz', default=False)
 
-	# bill_id = models.CharField(max_length=10, default='', blank=True)
-	# room = models.ForeignKey('regsoft.Room',null=True,blank = True)
-	class Meta:
-		verbose_name_plural = 'Participants'
-	def __unicode__(self):
-		return str(self.name)
+    # bill_id = models.CharField(max_length=10, default='', blank=True)
+    # room = models.ForeignKey('regsoft.Room',null=True,blank = True)
+    class Meta:
+        verbose_name_plural = 'Participants'
+    def __unicode__(self):
+        return str(self.name)
 
+# ToDo: Port this model to the EMS app next time
 class Team(models.Model):
-	name = models.CharField(max_length=200)
-	members = models.ManyToManyField('Participant')
-	leader = models.ForeignKey('Participant', related_name='leader_team')
-	event = models.ForeignKey('Event.Event')
-	def __unicode__(self):
-		return str(self.name)
+    # score_set
+    # levels
+    name = models.CharField(max_length=200)
+    members = models.ManyToManyField('Participant')
+    leader = models.ForeignKey('Participant', related_name='leader_team')
+    event = models.ForeignKey('Event.Event')
+    rank = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    def __unicode__(self):
+        return str(self.name)
 
 class College(models.Model):
-	name = models.CharField(max_length=200)
-	is_displayed = models.BooleanField(default=False)
-	def __unicode__(self):
-		return str(self.name)
+    name = models.CharField(max_length=200)
+    is_displayed = models.BooleanField(default=False)
+    def __unicode__(self):
+        return str(self.name)
 
 class Updates(models.Model):
     name = models.CharField(max_length=200)
--- ems/models.py
+++ ems/models.py
@@ -1,3 +1,67 @@
 from django.db import models
-
+from backend.models import Team
 # Create your models here.
+
+# The Team model is reflected here, to be ported next time.
+# class Team(models.Model):
+#   # score_set
+#   # levels
+# 	name = models.CharField(max_length=200)
+# 	members = models.ManyToManyField('Participant')
+# 	leader = models.ForeignKey('Participant', related_name='leader_team')
+# 	event = models.ForeignKey('Event.Event')
+#   rank = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+# 	def __unicode__(self):
+# 		return str(self.name)
+
+class Score(models.Model):
+    level = models.ForeignKey(Level)
+    team = models.ForeignKey(Team)
+    judge = models.ForeignKey(Judge)
+    var1 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    var2 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    var3 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    var4 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    var5 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    var6 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    var7 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    var8 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    var9 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+    var10 = models.PositiveSmallIntegerField(default=None, null=True, blank=True)
+
+class Level(models.Model):
+    # score_set
+    event = models.ForeignKey(Event)
+    label = models.ForeignKey(Label)
+    judges = models.ManyToManyField(Judge)
+    name = models.CharField(max_length=100)
+    position = models.PositiveSmallIntegerField()
+    teams = models.ManyToManyField(Team, related_name='levels')
+
+class Judge(models.Model):
+    name = models.CharField(max_length=200)
+    # score_set
+    # level_set
+
+class Label(models.Model):
+    # level_set
+    var1name = models.CharField(max_length=100)
+    var1max = models.PositiveSmallIntegerField(default=10)
+    var2name = models.CharField(max_length=100)
+    var2max = models.PositiveSmallIntegerField(default=10)
+    var3name = models.CharField(max_length=100)
+    var3max = models.PositiveSmallIntegerField(default=10)
+    var4name = models.CharField(max_length=100)
+    var4max = models.PositiveSmallIntegerField(default=10)
+    var5name = models.CharField(max_length=100)
+    var5max = models.PositiveSmallIntegerField(default=10)
+    var6name = models.CharField(max_length=100)
+    var6max = models.PositiveSmallIntegerField(default=10)
+    var7name = models.CharField(max_length=100)
+    var7max = models.PositiveSmallIntegerField(default=10)
+    var8name = models.CharField(max_length=100)
+    var8max = models.PositiveSmallIntegerField(default=10)
+    var9name = models.CharField(max_length=100)
+    var9max = models.PositiveSmallIntegerField(default=10)
+    var10name = models.CharField(max_length=100)
+    var10max = models.PositiveSmallIntegerField(default=10)
