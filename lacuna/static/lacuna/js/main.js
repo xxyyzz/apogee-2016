6dbe086ed9ef01607a2baad794de4ea5f00f04db
smitpatwa <smit.patwa@gmail.com>
2016-02-14 01:06:00 +0530

    all

--- lacuna/static/lacuna/js/main.js
+++ lacuna/static/lacuna/js/main.js
@@ -655,12 +655,12 @@ function statusChangeCallback(response) {
 		testAPI();
 	} else if (response.status === 'not_authorized') {
 	  // The person is logged into Facebook, but not your app.
-	  	Fb.login();
+	  	FB.login();
 		console.log('Please log ' + 'into this app.');
 	} else {
 	  // The person is not logged into Facebook, so we're not sure if
 	  // they are logged into this app or not.
-	  	Fb.login();
+	  	FB.login();
 		console.log('Please log ' + 'into Facebook.');
 	}
 }
