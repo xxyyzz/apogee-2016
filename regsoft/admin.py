7bd36118e6664380edd7ec8ab5466ec166e392fc
kunalsharma05 <ks05111996@gmail.com>
2016-02-14 16:57:42 +0530

    controlz reg

--- regsoft/admin.py
+++ regsoft/admin.py
@@ -5,16 +5,16 @@ from Event.models import *
 class RoomAdmin(admin.ModelAdmin):
 	readonly_fields=('id',)
 	list_display = ('id','room', 'bhavan','vacancy')
-# class BillAdmin(admin.ModelAdmin):
-	# readonly_fields=('id',)
-# 	list_display = ('id', 'gleader','college','amount')
+class BillAdmin(admin.ModelAdmin):
+	readonly_fields=('id',)
+	list_display = ('id', 'participant','amount')
 # class InventoryAdmin(admin.ModelAdmin):
 # 	readonly_fields=('id',)
 # 	list_display = ('id','gl_id','room','a','b','c','d','e','f')
 # class TeamAdmin(admin.ModelAdmin):
 #     list_display = ['leader', 'college']
 # #admin.site.register(Bill)
-# admin.site.register(Bill_new, BillAdmin)
+admin.site.register(Bill, BillAdmin)
 admin.site.register(Room, RoomAdmin)
 admin.site.register(Bhavan)
 
