91f153b26285dc89e28fc00291e443e9c9a4c06d
Nikhil Verma <noteenikhil@yahoo.in>
2016-02-14 19:34:13 +0530

    revengg

--- revengg/views.py
+++ revengg/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render
 from lacuna.models import *
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 # Create your views here.
 def software(request):
     return render(request, 'revengg/index.html')
 
-# @csrf_exempt
-# def user_login(request):
-#     name = request.POST['name']
-#     email = request.POST['email']
-#     phone = request.POST['phone']
-#     try:
-#         part = Participant.objects.get(email=email)
-#     except:
-#         part = Participant.objects.create(email=email, name=name, phone=phone)
-#     response = {
-#         'status' : 1,
-#     }
-#     return JsonResponse(response)
+@csrf_exempt
+def create(request):
+    name = request.POST['name']
+    email = request.POST['email']
+    phone = request.POST['phone']
+    try:
+        Participant.objects.get(email=email)
+    except:
+        Participant.objects.create(name=name, email=email, phone=phone)
+    return HttpResponse('1')
 
+@csrf_exempt
+def update(request):
+    score = request.POST['name']
+    email = request.POST['email']
+    time = request.POST['phone']
+    part = Participant.objects.get(email=email)
+    part.time = time
+    part.score = score
+    part.save()
+    return HttpResponse('1')
