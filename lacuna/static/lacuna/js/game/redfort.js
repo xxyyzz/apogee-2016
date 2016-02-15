3cc4dc916fa064d4655efe3eba83264bd6245a5a
smitpatwa <smit.patwa@gmail.com>
2016-02-15 17:54:34 +0530

    latitude longitude checking only upto 2 decimal

--- lacuna/static/lacuna/js/game/redfort.js
+++ lacuna/static/lacuna/js/game/redfort.js
@@ -1,6 +1,10 @@
 function redfortinit(){
 	$('#rfort_submit').click(function(){
 		var tp=$('#rfort_input').val();
+		var x = tp.split(",");
+		x[0]=parseFloat(x[0]).toFixed(2);
+		x[1]=parseFloat(x[1]).toFixed(2);
+		tp=x[0]+","+x[1];
 		submit_ans(tp,6);
 	});
 }
