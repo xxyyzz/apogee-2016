545c51aecdc3a2e39da7d75c3bcc67a85081c1e6
Nikhil Verma <noteenikhil@yahoo.in>
2016-02-18 16:58:38 +0530

    migrations

---
+++ Event/migrations/0046_auto_20160218_1656.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('Event', '0045_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='org',
+            field=models.ForeignKey(default=None, blank=True, to='Event.Organization', null=True),
+        ),
+    ]
