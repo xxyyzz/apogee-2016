e56faf4a35a430efb892d605b0dc23d089bc0cfe
Nikhil Verma <noteenikhil@yahoo.in>
2016-02-18 15:05:07 +0530

    merge migratioins

---
+++ Event/migrations/0045_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('Event', '0044_auto_20160218_0020'),
+        ('Event', '0044_merge'),
+    ]
+
+    operations = [
+    ]
