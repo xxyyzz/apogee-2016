2e203f163db7ca282b1f3da1fb67494215a26f2c
Nikhil Verma <noteenikhil@yahoo.in>
2016-02-19 20:36:52 +0530

    admin changes

--- backend/admin.py
+++ backend/admin.py
@@ -9,8 +9,8 @@ class ParticipantAdmin(admin.ModelAdmin):
     filter_horizontal = ['events', 'teams']
     search_fields = ['name']
     list_filter = ['is_bitsian', 'college']
-    list_display = ['name', 'college', 'is_bitsian', 'aadhaar', 'college']
-    list_editable = ['is_bitsian', 'aadhaar', 'college']
+    list_display = ['name', 'is_bitsian', 'aadhaar']
+    list_editable = ['is_bitsian', 'aadhaar']
 
 
 admin.site.register(Participant, ParticipantAdmin)
