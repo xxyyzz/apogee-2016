6d4ff70bab376125a2563af6d5b6d31835fdfff7
satwik77 <satwik55@gmail.com>
2016-02-22 00:23:48 +0530

    migrations merge

---
+++ backend/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('backend', '0011_participant_barcode'),
+        ('backend', '0011_auto_20160221_1553'),
+    ]
+
+    operations = [
+    ]
