30bf06cb2c16fdea54c24c56300458499e3f6ad6
satwik77 <satwik55@gmail.com>
2016-03-30 23:03:38 +0530

    aarohan results

--- aarohan/admin.py
+++ aarohan/admin.py
@@ -3,3 +3,5 @@ from django.contrib import admin
 class ResultAdmin(admin.ModelAdmin):
 	readonly_fields=('id',)
 	list_display = ('id','name','roll_no', 'standard','national_rank')
+
+admin.site.register(Results, ResultAdmin)
