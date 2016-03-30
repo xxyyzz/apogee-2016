6b01c7211aee5390c6673ababdd2875c01820742
satwik77 <satwik55@gmail.com>
2016-03-30 23:17:04 +0530

    update

--- aarohan/views.py
+++ aarohan/views.py
@@ -20,3 +20,13 @@ def soft11(request) :
 
 def soft12(request) :
 	return render(request, "aarohan/soft12.html")
+
+def results(request):
+	if request.POST:
+		roll_no= int(request.POST['roll'])
+		try:
+			res_ob= Results.objects.get(roll_no=roll_no)
+		except:
+			return HttpResponse('Error: Not Found')
+		return render(request,'../../pcradmin/templates/pcradmin/res_details.html',{'res':res_ob})
+	return render(request,'../../pcradmin/templates/pcradmin/aarohan_results.html')
