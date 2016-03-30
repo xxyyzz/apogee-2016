57bc1c3edcad55511edadf800992a5e415e8453d
satwik77 <satwik55@gmail.com>
2016-03-30 23:25:21 +0530

    fix

--- aarohan/urls.py
+++ aarohan/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^software/cl10', views.soft10),
     url(r'^software/cl11', views.soft11),
     url(r'^software/cl12', views.soft12),
-    url(r'^aarohan_results', views.results),
+    url(r'^aarohan_results/$', views.results),
 
     ]
